b6d92de63226f3b4f2bc21da4fc66e984eadfa2a
강 <ghang0501@gmail.com>
2023-04-10 21:37:50 +0900

    range없이 timeLine 재생 기능 완료

--- src/client/js/songPlay.js
+++ src/client/js/songPlay.js
@@ -4,6 +4,9 @@ const totalTime = document.querySelector(".playSong__totalTime");
 const viewingTime = document.querySelector(".playSong__viewingTime");
 const timeLine = document.querySelector(".playSong__timeLine");
 const songPlayDiv = document.querySelector(".songPlayBtnBgDiv");
+const timeLineListening = document.querySelector(
+  ".playSong__timeLine__listening"
+);
 
 let songPlayBtnChang = false;
 
@@ -45,6 +48,10 @@ const handleTimeUpdate = () => {
   playCheck();
   totalTime.innerText = formatTime(Math.floor(audio.currentTime));
   timeLine.value = Math.floor(audio.currentTime);
+  const progress = audio.currentTime / audio.duration; // 재생 시간의 진행률 계산
+  const width = timeLine.offsetWidth * progress; // 타임라인 요소의 너비에 진행률을 곱하여 playback 요소의 너비 계산
+
+  timeLineListening.style.width = width + "px"; // playback 요소의 너비 설정
 };
 const handleTimelineChange = (event) => {
   const {
@@ -66,9 +73,11 @@ const handleSongPlayAClick = (event) => {
   }
   audioPlay();
 };
+
 songPlayIcon.addEventListener("click", handleSongPlayIconClick);
 audio.addEventListener("loadedmetadata", handleLoadedMetadata);
 audio.addEventListener("timeupdate", handleTimeUpdate);
 timeLine.addEventListener("input", handleTimelineChange);
 window.addEventListener("keydown", handleKeyDown);
 songPlayDiv.addEventListener("click", handleSongPlayAClick);
+//timeLine.on("click", playFromClickedPos);
